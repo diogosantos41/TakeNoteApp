2957f890873ac8a6afc31baf1da0b7c0049cf469
Diogo Santos <diogocastrosantos@gmail.com>
2022-06-26 00:35:00 +0100

    Clean Architecture Packages + DB + Note Screen
    + dependencies
    + clean architecture project packages + database implementation and data classes
    + view model
    + compose note screen

---
+++ app/src/main/java/com/dscoding/takenoteapp/di/AppModule.kt
@@ -0,0 +1,44 @@
+package com.dscoding.takenoteapp.di
+
+import android.app.Application
+import androidx.room.Room
+import com.dscoding.takenoteapp.feature_note.data.data_source.NoteDatabase
+import com.dscoding.takenoteapp.feature_note.data.repository.NoteRepositoryImpl
+import com.dscoding.takenoteapp.feature_note.domain.repository.NoteRepository
+import com.dscoding.takenoteapp.feature_note.domain.use_case.AddNoteUseCase
+import com.dscoding.takenoteapp.feature_note.domain.use_case.DeleteNoteUseCase
+import com.dscoding.takenoteapp.feature_note.domain.use_case.GetNotesUseCase
+import com.dscoding.takenoteapp.feature_note.domain.use_case.NoteUseCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDatabase {
+        return Room.databaseBuilder(app, NoteDatabase::class.java, NoteDatabase.DATABASE_NAME)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NoteUseCases {
+        return NoteUseCases(
+            getNotes = GetNotesUseCase(repository),
+            deleteNote = DeleteNoteUseCase(repository),
+            addNote = AddNoteUseCase(repository)
+        )
+    }
+}
--- app/src/main/java/com/dscoding/takenoteapp/ui/theme/Color.kt
+++ app/src/main/java/com/dscoding/takenoteapp/ui/theme/Color.kt
@@ -6,3 +6,9 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+
+val pink = Color(0xFFFFC0CB)
+val red = Color (0xFFFF7F7F)
+val blue = Color(0xFF87CEEB)
+val green = Color(0xFFAED9B2)
